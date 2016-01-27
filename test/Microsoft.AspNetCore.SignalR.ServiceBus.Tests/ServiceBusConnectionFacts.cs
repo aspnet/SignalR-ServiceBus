cbe74ccbaa74218251940bb03829dc2892517d7e
Brennan <brecon@microsoft.com>
2016-01-27 15:43:27 -0800

    React to Logging API changes

--- test/Microsoft.AspNetCore.SignalR.ServiceBus.Tests/ServiceBusConnectionFacts.cs
+++ test/Microsoft.AspNetCore.SignalR.ServiceBus.Tests/ServiceBusConnectionFacts.cs
@@ -23,7 +23,7 @@ namespace Microsoft.AspNetCore.SignalR.ServiceBus.Tests
                 throw new NotImplementedException();
             }
 
-            public void Log(LogLevel logLevel, int eventId, object state, Exception exception, Func<object, Exception, string> formatter)
+            public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
             {
             }
         }
